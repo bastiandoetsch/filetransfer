7b560dc884cdc55dbb86dc81b609d51548014f6d
Bastian Doetsch <bastian.doetsch@gmx.de>
2018-09-09 13:57:58 +0200

    init

---
+++ src/main/java/info/doetsch/filetransfer/FiletransferApplication.java
@@ -0,0 +1,12 @@
+package info.doetsch.filetransfer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiletransferApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FiletransferApplication.class, args);
+	}
+}
